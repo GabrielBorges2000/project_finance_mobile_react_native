ceac4529f6f9f587e207c396f515ee6d736a0726
Gabriel Borges <gabriel.vscode@gmail.com>
2022-10-18 16:28:22 -0300

    Gabriel
    Commit initial, aqui eu realizei a criação do projeto e instalação do TypeScrip e Styled Components e a typagem do Styled Components.

---
+++ App.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Welcome } from './src/components/Welcome';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Welcome title='React Native Bare Workflow com TypeScript!'/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
