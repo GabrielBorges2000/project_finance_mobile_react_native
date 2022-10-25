e48e3694c6e1c96f2e0fc7d81d40276156141b60
Gabriel Borges <gabriel.vscode@gmail.com>
2022-10-25 16:36:25 -0300

    Gabriel
    teste de Rotas

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+/* import { DefaultLayout } from './layouts/DefaultLayouts' */
+import { Register } from './screens/Register'
+import { Dashboard } from './screens/Dashboard'
+
+export function Router(){
+  return(
+     <Routes>
+         <Route path="/" /* element={<DefaultLayout/>} */>
+            <Route path="/" element={<Dashboard/>}/>
+            <Route path="/history" element={<Register/>}/>
+         </Route>
+     </Routes>
+  )
+}
